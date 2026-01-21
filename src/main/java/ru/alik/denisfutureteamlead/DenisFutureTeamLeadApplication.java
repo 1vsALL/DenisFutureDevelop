252e634215ac2f9c514373d846f3c0fec6806cc0
1vsALL <alikkv@mail.ru>
2026-01-21 22:55:14 +0300

    добавляет класс для начала

--- src/main/java/ru/alik/denisfutureteamlead/DenisFutureTeamLeadApplication.java
+++ src/main/java/ru/alik/denisfutureteamlead/DenisFutureTeamLeadApplication.java
@@ -1,13 +1,13 @@
-package ru.alik.denisfutureteamlead;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DenisFutureTeamLeadApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DenisFutureTeamLeadApplication.class, args);
-	}
-
-}
+//package ru.alik.denisfutureteamlead;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class DenisFutureTeamLeadApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DenisFutureTeamLeadApplication.class, args);
+//	}
+//
+//}
