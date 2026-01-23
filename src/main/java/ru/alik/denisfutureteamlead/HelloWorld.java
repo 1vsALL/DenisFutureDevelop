48ed8f695a12685c14b68796545e9ef6f48f1da7
1vsALL <alikkv@mail.ru>
2026-01-23 10:40:25 +0300

    добавляет коммент

--- src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
+++ src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
@@ -3,5 +3,6 @@ package ru.alik.denisfutureteamlead;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to IT");
+        //когда таска закроется?
     }
 }
