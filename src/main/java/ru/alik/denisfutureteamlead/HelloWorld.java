6dc8f5e23d8a77eee4d3ad632b7c707989b308cd
1vsALL <alikkv@mail.ru>
2026-02-04 22:03:59 +0300

    инициирует его

--- src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
+++ src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to IT");
         //когда таска закроется?
-        String string;
+        String string =new String();
     }
 }
