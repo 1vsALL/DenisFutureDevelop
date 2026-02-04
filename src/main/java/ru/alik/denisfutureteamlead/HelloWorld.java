0d229652e0352f909b3e710b068f47509c71f960
1vsALL <alikkv@mail.ru>
2026-02-04 23:19:24 +0300

    чо то как то заново всё

---
+++ src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
@@ -0,0 +1,9 @@
+package ru.alik.denisfutureteamlead;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to IT");
+        //когда таска закроется?
+        String string = new String();
+    }
+}
---
+++ src/test/java/ru/alik/denisfutureteamlead/DenisFutureTeamLeadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alik.denisfutureteamlead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenisFutureTeamLeadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
