4798b8405fbe028a1aec0faaf116ec19b9101ea5
1vsALL <alikkv@mail.ru>
2026-02-10 20:37:22 +0300

    тест очередность ПР в одну ветку

---
+++ src/main/java/ru/alik/denisfutureteamlead/Currencies.java
@@ -0,0 +1,26 @@
+package ru.alik.denisfutureteamlead;
+
+import java.util.ArrayList;
+
+public enum Currencies {
+    RUB("RUB"),
+    USD("USD"),
+    EUR("EUR"),
+    CNY("CNY");
+    private final String code;
+
+    Currencies(String code) {
+        this.code = code;
+    }
+    public String getCode() {
+        return code;
+    }
+    public static ArrayList<String> getCurrencies() {
+        ArrayList<String> currencies = new ArrayList<>();
+        currencies.add(RUB.getCode());
+        currencies.add(USD.getCode());
+        currencies.add(EUR.getCode());
+        currencies.add(CNY.getCode());
+        return currencies;
+    }
+}
--- src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
+++ src/main/java/ru/alik/denisfutureteamlead/HelloWorld.java
@@ -1,9 +1,15 @@
 package ru.alik.denisfutureteamlead;
 
+import java.util.List;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to IT");
         //когда таска закроется?
         String string = new String();
+        List<String> list = Currencies.getCurrencies();
+        System.out.println(list);
+        // первый ПР
     }
+
 }
